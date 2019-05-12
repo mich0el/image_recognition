ae2ff94b5972a5b242fa3d80b79a517dd4f702c5
Michael <mihaaasss@gmail.com>
2019-05-12 02:46:06 +0200

    fully trained model and almost finished user app

--- user_app.py
+++ user_app.py
@@ -6,49 +6,60 @@ import sys
 from keras.preprocessing.image import ImageDataGenerator
 import shutil
 import os
-
+import cv2
 from PIL import Image
 import numpy as np
-
 from keras import utils
 import numpy
-
-
-
-
-def image_converter(path_to_image):
-    pass
 
 
 if __name__ == '__main__':
     Tk().withdraw()
 
+    #load a model
     try:
         model = load_model('model.h5')
     except:
         messagebox.showerror("Error", "No \"model.h5\" file, please train a new model or place an existing model file in directory with this program and name it \"model.h5\".")
         sys.exit()
 
+    #read file path
     messagebox.showinfo("Welcome!", "Please, select JPG/JPEG image for recognition in the next step. \n\nAvaliable animals: bear, cat, dog, elephant, giraffe, gorilla, owl, parrot, penguin, zebra.")
     filename = askopenfilename()
 
-    if filename and ('jpg' in filename or 'jpeg' in filename):
-        print("copying")
+    #save resized tmp image
+    if filename and ('jpg' in filename or 'jpeg' or 'png' in filename):
         os.makedirs("./user_img/dog")
-        shutil.copyfile(filename, "./user_img/dog/tmp_file.jpg")
+        img = cv2.imread(filename)
+        resized_img = cv2.resize(img, (256, 256))
+        cv2.imwrite("./user_img/dog/tmp_fildddde.jpg", resized_img)
     else:
         messagebox.showerror("File error!", "Wrong file type (only JPG/JPEG acceptable).")
         sys.exit()
 
 
-    recognize_batch = ImageDataGenerator().flow_from_directory("user_img/", target_size=(256, 256), classes=['cat', 'dog'], batch_size=2)
-    imgs, labels = next(recognize_batch)
+    recognize_batch = ImageDataGenerator().flow_from_directory("user_img/", target_size=(256, 256), classes=['bear', 'cat', 'dog', 'elephant', 'giraffe', 'gorilla', 'owl', 'parrot', 'penguin', 'zebra'], batch_size=1)
+    #imgs, labels = next(recognize_batch)
 
+    animal_strings = [
+        'bear',
+        'cat',
+        'dog',
+        'elephant',
+        'giraffe',
+        'gorilla',
+        'owl',
+        'parrot',
+        'penguin',
+        'zebra'
+    ]
 
-    predictions = model.predict_generator(recognize_batch, steps=1, verbose=0)
+    predictions = model.predict_generator(recognize_batch, steps=1, verbose=1)
     print(predictions)
 
-    result = numpy.amax(predictions)
-    messagebox.showinfo("Result", "with probability = " + str(round(result * 100, 2)) + "% it is " + "cat" if predictions[0][0] > predictions[0][1] else "dog")
+
+
+    max_index = numpy.where(predictions == numpy.amax(predictions))
+    messagebox.showinfo("Result", "with probability = " + str(round(numpy.amax(predictions) * 100, 2)) + "% it is: " + animal_strings[int(max_index[1])])
 
     shutil.rmtree("./user_img/")
